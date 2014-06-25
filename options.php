8acbbe3018003d8e1c121cdef21d693b2ed212be
tytus-tytus <tytus-tytus@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-06-25 06:09:45 +0000

    update
    git-svn-id: https://plugins.svn.wordpress.org/tidio-live-chat/trunk@938252 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- options.php
+++ options.php
@@ -1,5 +1,3 @@
 <h3>Tidio Live Chat</h3>
-
 <p>Click "Go to Live Chat" to continue to your chat panel.</p>
-
 <a href="<?php echo "http://external.tidiochat.com/access?privateKey=".self::getPrivateKey(); ?>" class="button button-primary" target="_blank">Go to Live Chat</a>
